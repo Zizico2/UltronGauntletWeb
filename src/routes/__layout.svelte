3f1e9a73d311cc64df2f420e77f351207df21c2c
Zizico2 <berna.agua@gmail.com>
2022-06-05 04:08:11 +0100

    headlessui test

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,29 @@
 <script lang="ts">
 	import '../app.css';
+	import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@rgossiaux/svelte-headlessui';
 </script>
 
-<header>I'm header</header>
-
-<main>
-	<slot />
-</main>
-
-<footer>I'm footer</footer>
+<TabGroup>
+	<TabList>
+		<a href="/"><Tab>Tab 1</Tab></a>
+		<a href="/tab2"><Tab>Tab 2</Tab></a>
+		<a href="/tab3"><Tab>Tab 3</Tab></a>
+	</TabList>
+	<TabPanels>
+		<TabPanel
+			><main>
+				<slot />
+			</main></TabPanel
+		>
+		<TabPanel
+			><main>
+				<slot />
+			</main></TabPanel
+		>
+		<TabPanel
+			><main>
+				<slot />
+			</main></TabPanel
+		>
+	</TabPanels>
+</TabGroup>
